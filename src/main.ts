db6d0c251d51d179929a8bb5caf9d58bbc096c1a
AvhikBiswas <abhikbiswas351@gmail.com>
2024-08-12 23:54:43 +0530

    rate limiting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', true);
   await app.listen(3000);
 }
 bootstrap();
